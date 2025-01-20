e3ada9b184adba4b7a1e5d92431aec16e4b10b12
Andrew Hughes <andyashmore1@gmailcom>
2025-01-20 12:00:37 -0600

    create entity classes

---
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package pet.store;
+
+/**
+ * 
+ */
+public class PetStoreApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/pet/store/entity/PetStore.java
+++ src/main/java/pet/store/entity/PetStore.java
@@ -1,10 +1,11 @@
+/**
+ * 
+ */
 package pet.store.entity;
 
-import jakarta.persistence.Entity;
-import lombok.Data;
-
-@Entity
-@Data
+/**
+ * 
+ */
 public class PetStore {
 
 }
