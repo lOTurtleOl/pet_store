00b096e3f55ae76df0daa249bc41fdd90d777655
Andrew Hughes <andyashmore1@gmailcom>
2025-01-20 19:54:49 -0600

    clean up code

--- src/main/java/pet/store/PetStoreApplication.java
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -3,16 +3,14 @@
  */
 package pet.store;
 
+import org.springframework.boot.SpringApplication;
+
 /**
  * 
  */
 public class PetStoreApplication {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(PetStoreApplication.class, args);
 
 	}
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (customer) VALUES ('Andrew', 'Hughes', 'ah@gmail.com');
+INSERT INTO customer (customer) VALUES ('Michaela', 'Hughes', 'mh@gmail.com')
+INSERT INTO customer (customer) VALUES ('Aurora', 'Hughes', 'arh@gmail.com')
+INSERT INTO customer (customer) VALUES ('Kelly', 'Mortimer', 'kmortimer@gmail.com')
+INSERT INTO customer (customer) VALUES ('John', 'Mortimer', 'johnmortimer@gmail.com')
